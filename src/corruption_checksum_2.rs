b8c5e2a55d9050767b2d968a59c666ca5febffec
errantsky <dev@errantsky.dev>
2021-06-01 13:43:55 -0400

    Added solutions for day 1 and 2. Learned about split_whitespace, char's to_digit and String's parse methods.

---
+++ src/corruption_checksum_2.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn find_checksum() -> u32 {
+    let path = "day2.txt";
+    let file = File::open(path).unwrap();
+    let buffered = BufReader::new(file);
+    buffered.lines().map(|line| {
+        let list = line.unwrap()
+            .split_whitespace()
+            .map(|num_str| num_str.parse::<u32>().unwrap())
+            .collect();
+
+        list.iter().max().unwrap() - list.iter().min().unwrap()
+    }).sum()
+}
+
+fn find_checksum2() -> u32 {
+    let path = "day2.txt";
+    let file = File::open(path).unwrap();
+    let buffered = BufReader::new(file);
+    buffered.lines().map(|line| {
+        let list: Vec<u32> = line.unwrap()
+            .split_whitespace()
+            .map(|num_str| num_str.parse::<u32>().unwrap())
+            .collect();
+
+        let mut res: u32 = 0;
+        for i in 0..list.len() {
+            for j in 0..list.len() {
+                if i != j && list[i] % list[j] == 0 {
+                    res = list[i] / list[j]
+                }
+            }
+        }
+        res
+    }).sum()
+}
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+
+    #[test]
+    fn sample1_test1() {
+
+    }
+
+    #[test]
+    fn submission1_test() {
+        println!("Part 1 answer: {}", find_checksum());
+    }
+
+    #[test]
+    fn sample2_test1() {
+
+    }
+
+    #[test]
+    fn submission2_test() {
+        println!("Part 2 answer: {}", find_checksum2());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod inverse_captcha_1;
+mod corruption_checksum_2;
 
 fn main() {
     println!("Hello, world!");
